7a7a3eb2fb3c90b2211fe8a98121dbee4acf7446
kuzmichit <kuzmichit@gmail.com>
2024-08-08 07:00:46 +0200

    commit save point

--- src/CocktailApp/Virtual_slides.jsx
+++ src/CocktailApp/Virtual_slides.jsx
@@ -11,25 +11,26 @@ import CardCocktail from './Card.jsx';
 import images from './Slides'
 
 export default () => {
-  // Create array with 1000 slides
+
   const listDrinks = drinks.drinks;
   const list = new Set();
+
   listDrinks.forEach(i => {
-    
-    list.add(i);
-    
-    } );
-    console.log(list.keys())
+    list.add(i.strCategory);
+  });
+  
+    console.log(list)
 
 const getSrcImage = (path = []) => {
   let src = path.split('/')
-  src = src[src.length - 1]
+  src = './' + src[src.length - 1]
   return src
 }
-let images = require.context('./Slides', true)
-const imageList = images.keys().map(image => (
-  { image: images(image) } ) );
-console.log(imageList[1])
+let slideImages = require.context('./Slides', true)
+const imageList = slideImages.keys().map(image => (
+  { [`${image}`]: slideImages(image) }));
+  
+  console.log(imageList,'imageList');
 
   return (
     <Swiper 
@@ -43,7 +44,7 @@ console.log(imageList[1])
       {listDrinks.map((item, index) => (
         <SwiperSlide key={item.idDrink} virtualIndex={index} style={{ border: 'none !important' }} >
           {<CardCocktail
-            src={imageList[index]}
+            src={item[getSrcImage(item.strDrinkThumb)]}
             title={item.strDrink}
             text={item.strInstructionsIT}
           />}
