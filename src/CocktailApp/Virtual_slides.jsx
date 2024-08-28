b7691f6c80b8d50b4426cd9efe02491bdefdd2e6
kuzia <kuzmichit@gmail.com>
2024-08-28 13:33:55 +0200

    update Virtual_slides.jsx

--- src/CocktailApp/Virtual_slides.jsx
+++ src/CocktailApp/Virtual_slides.jsx
@@ -12,41 +12,53 @@ import images from './Slides'
 
 export default () => {
 
-  const listDrinks = drinks.drinks;
+  const listDrinks = drinks.list;
   const list = new Set();
-  
 
-const getSrcImage = (path = []) => {
-  let src = path.split('/')
-  src = './' + src[src.length - 1]
-  return src
-}
-let slideImages = require.context('./Slides', true)
-const imageList = slideImages.keys().map(image => (
-  { [`${image}`]: slideImages(image) }));
-  
-  
-  function clg() {
-   for (var key in imageList) {
-     console.log(key)
-   }
- }
+  listDrinks.forEach(i => {
+    list.add(i.strCategory);
+  });
+
+  // la lista delle bibite per fare il filter
+  const filter = ["Cocktail", "Ordinary Drink", "Shot", "Punch / Party Drink", "Coffee / Tea", "Beer"]
+
+  const getSrcImage = (path = []) => {
+    let src = path.split('/')
+    src = './' + src[src.length - 1]
+    return src
+  }
+
+  let slideImages = require.context('./Slides', true)
+  const objectSlides = {};
+  const imageList = slideImages.keys().forEach(image => (
+    objectSlides[`${image}`] = slideImages(image)));
+
+  console.log(slideImages);
+  // imageList.forEach(item => {
+
+  //   for (const key in item) {
+  //     if (Object.hasOwnProperty.call(item, key)) {
+  //       const element = item[key];
+
+  //       console.log(element);
+  //     }
+
+  //   }
+  // } )
 
   return (
     <Swiper 
       modules={[Virtual, Navigation]}
       navigation
       onSlideChange={() => console.log('slide change')}
-      onSwiper={(swiper) => console.log('swiper')}
+      onSwiper={(swiper) => console.log(swiper)}
     spaceBetween={50}
     slidesPerView={1}
     virtual>
       {listDrinks.map((item, index) => (
-        <SwiperSlide key={item.idDrink} virtualIndex={index} style={{ border: 'none !important' }} 
-          onClick={clg()} >
-        
+        <SwiperSlide key={item.idDrink} virtualIndex={index} style={{ border: 'none !important' }} >
           {<CardCocktail
-            src={slideImages[getSrcImage(item.strDrinkThumb)]}
+            src={objectSlides[getSrcImage(item.strDrinkThumb)]}
             title={item.strDrink}
             text={item.strInstructionsIT}
           />}
