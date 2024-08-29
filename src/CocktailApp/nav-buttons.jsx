182dc79943df0c337f6f71426808965d6f9a0e40
kuzmichit <kuzmichit@gmail.com>
2024-08-29 07:05:18 +0200

    deploy 27/08

--- src/CocktailApp/nav-buttons.jsx
+++ src/CocktailApp/nav-buttons.jsx
@@ -3,9 +3,9 @@ import React from "react";
 
 const NavButtons = () => {
 
-finire style, aggiungere logica 
+//finire style, aggiungere logica 
   return ( <Row>
-    <Col><Button type='button' id='btnPrev'>Precedente</Button></Col>
+    <Col className='w-50 color-primary btn-light'><Button className='btn-light w-50' type='button' id='btnPrev'>Precedente</Button></Col>
     <Col><Button type="button" id='btnNext'>Successivo</Button></Col>
   </Row>
   )
