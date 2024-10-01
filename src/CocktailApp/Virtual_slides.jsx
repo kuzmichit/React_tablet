cb0d2ed4fe253c40a4787ee7820745afd6e0f227
kuzmichit <kuzmichit@gmail.com>
2024-10-01 07:00:11 +0200

    update

--- src/CocktailApp/Virtual_slides.jsx
+++ src/CocktailApp/Virtual_slides.jsx
@@ -1,34 +1,41 @@
 import React from "react";
-import { Virtual, Navigation } from 'swiper/modules';
-import { Swiper, SwiperSlide } from 'swiper/react';
+import { Virtual, Navigation } from "swiper/modules";
+import { Swiper, SwiperSlide } from "swiper/react";
 
 // Import Swiper styles
-import 'swiper/css';
-import 'swiper/css/virtual';
+import "swiper/css";
+import "swiper/css/virtual";
 
-import drinks from './Drinks.json'
-import CardCocktail from './Card.jsx';
-import images from './Slides'
+import drinks from "./Drinks.json";
+import CardCocktail from "./Card.jsx";
+import images from "./Slides";
 
 export default () => {
+	const listDrinks = drinks.list;
 
-  const listDrinks = drinks.list;
+	// la lista delle bibite per fare il filter
+	const filter = [
+		"Cocktail",
+		"Ordinary Drink",
+		"Shot",
+		"Punch / Party Drink",
+		"Coffee / Tea",
+		"Beer",
+	];
 
-  // la lista delle bibite per fare il filter
-  const filter = ["Cocktail", "Ordinary Drink", "Shot", "Punch / Party Drink", "Coffee / Tea", "Beer"]
+	const getSrcImage = (path = []) => {
+		let src = path.split("/");
+		src = "./" + src[src.length - 1];
+		return src;
+	};
 
-  const getSrcImage = (path = []) => {
-    let src = path.split('/')
-    src = './' + src[src.length - 1]
-    return src
-  }
+	let slideImages = require.context("./Slides", true);
+	const objectSlides = {};
+	slideImages
+		.keys()
+		.forEach(image => (objectSlides[image] = slideImages(image)));
 
-  let slideImages = require.context('./Slides', true)
-  const objectSlides = {};
-  slideImages.keys().forEach(image => (
-    objectSlides[image] = slideImages(image)));
-
-  return (
+	return (
 		<Swiper
 			modules={[Virtual, Navigation]}
 			navigation={{
